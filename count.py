83e670316e58f29f0e6b83690fcfd5ab96bb5a89
luojiyin <luojiyin@luojiyindeMacBook-Pro.local>
2016-09-21 23:37:39 +0800

    初步完成分词，下一步要统计词频

---
+++ count.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+import jieba
+
+file = open("word.txt")
+
+while 1:
+    line = file.readline()
+    print line
+    fullMode = jieba.cut(line, cut_all=True)
+    defaultMode = jieba.cut(line, cut_all=False)
+    searchMode = jieba.cut_for_search(line)
+
+    print "全模式: \n",' '.join(fullMode)
+    print "精确模式: \n", ' '.join(defaultMode)
+    print '搜索引擎模式: \n',' '.join(searchMode)
+    if not line:
+        print "finish"
+        break
+    
