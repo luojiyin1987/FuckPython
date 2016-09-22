1e8f4e05a995511794cc3c366c40747b3312117b
luojiyin <luojiyin@luojiyindeMacBook-Pro.local>
2016-09-22 23:03:42 +0800

    test decode

--- count.py
+++ count.py
@@ -1,19 +1,30 @@
 #coding=utf-8
 import jieba
+from collections  import Counter
+
 
 file = open("word.txt")
 
 while 1:
     line = file.readline()
     print line
-    fullMode = jieba.cut(line, cut_all=True)
-    defaultMode = jieba.cut(line, cut_all=False)
-    searchMode = jieba.cut_for_search(line)
+    words = [word for word in jieba.cut(line, cut_all=True) if len(word)>=2]
+    c = Counter(words)
+
+
+    for word_freq in c.most_common(10):
+        word, freq = word_freq
+        print
+
+
+
+    #defaultMode = jieba.cut(line, cut_all=False)
+    #searchMode = jieba.cut_for_search(line)
 
    #print "全模式: \n",' '.join(fullMode)
-    print "精确模式: \n", ' '.join(defaultMode)
+   #print "精确模式: \n", ' '.join(defaultMode)
    # print '搜索引擎模式: \n',' '.join(searchMode)
     if not line:
         print "finish"
         break
-file.close()    
+file.close()
