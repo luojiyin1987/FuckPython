135bafe2acd0b56b81be2caa4a84f0c330f8ac3c
luojiyin <luojiyin@hotmail.com>
2016-09-24 17:27:32 +0800

    优化算法

--- countWord.py
+++ countWord.py
@@ -6,6 +6,7 @@ def readflie():
     wordlist=[]
     templist=[]
     wordcount={}
+    tempdict={}
     base=open('EnglishWord.txt', 'r')
     baseinfo=base.readlines()
     for  i in baseinfo:
@@ -24,21 +25,20 @@ def readflie():
     base.close()
 
     for word in wordlist:
+
         if word not in templist:
             templist.append(word)
+            wordcount[word]=1
+        if word in templist:
+            wordcount[word]=wordcount[word]+1
+
+    for word in   wordcount:
+        print  '%s 次数是  %d' %(word, wordcount[word])
 
 
-    for tempword in templist:
-        i=0
-        for word in wordlist:
 
-            if word == tempword:
-                    i=i+1
-                    print i
-                    wordcount[tempword]=i
-
-    for   word in wordcount:
-        print   '%s, value=%d' %(word, wordcount[word])
+    #for   word in wordcount:
+    #    print   '%s, value=%d' %(word, wordcount[word])
 
 if __name__ == "__main__":
     readflie()
