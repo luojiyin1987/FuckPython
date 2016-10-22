c8a55921250777b3fd8fefdd18ccc28650902c9a
luojiyin1987 <luojiyin@hotmail.com>
2016-10-23 00:19:39 +0800

    test

--- multi_thread.py
+++ multi_thread.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python
-
 from threading import Thread
 import time
 
-def my_counter():
-    i = 0
-    for  _ in range(100000000):
         i = i + 1 
     return True
 
@@ -13,13 +9,13 @@ def main():
     thread_array = {}
     start_time = time.time()
     for tid in range(2):
-        t = Thread(target = my_counter)
-        t.start()
-        thread_array[tid] = t
-    for i in range(2):
-        thread_array[i].join()
-    end_time = time.time()
-    print ("Total time : {}".format(end_time - start_time))
+    --    t = Thread(target = my_counter)
+    --    t.start()
+    --    thread_array[tid] = t
+    --for i in range(2):
+    --    thread_array[i].join()
+    --end_time = time.time()
+    print ("Total tim_time))
 
 if __name__ == '__main__':
     main()
