880f8a0653118a54ca1dcb2318579003d939752b
luojiyin <luojiyin@hotmail.com>
2016-11-21 07:13:12 -0800

    连上数据库了，密码记错了, 下一步解决sql拼写和防注入问题。不用框架，自己手写还是有很多细节问题要考虑

--- random_test.py
+++ random_test.py
@@ -1,8 +1,81 @@
 #!/usr/bin/env python
 #coding:utf-8
 import string , random
+import MySQLdb
 
 fild = string.letters + string.digits
+
+class mysql_init(object):
+    def __init__(self, conn):
+        self.conn = None
+
+    #connect to mysql
+    def connect(self):
+        self.conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user="root",
+            passwd="vizrtluo1987",
+            db="reboot",
+            charset="utf8"
+        )
+
+    def cursor(self):
+        try:
+            return self.conn.cursor()
+        except(AttributeError, MySQLdb.OperationalError):
+            self.connect()
+            return self.conn.cursor()
+
+    def commit(self):
+        return self.conn.commit()
+
+    def close(self):
+        return self.conn.close()
+
+def process():
+    dbconn.connect()
+    conn = dbconn.cursor()
+    DropTable(conn)
+    CreateTable(conn)
+    InsertDatas(conn)
+    QueryDatas(conn)
+    dbconn.close()
+
+def query(sql, conn):
+    conn.execute(sql)
+    rows = conn.fetchall()
+    return rows
+
+def DropTable(conn):
+    conn.execute("DROP TABLE IF EXISTS 'user_key'") 
+
+def CreateTable(conn):
+    sql_create = '''CREATE TABLE 'user_key' ('key' varchar(50) NOT null)'''
+    conn.execute(sql_create)
+
+def InserDates(conn):
+    insert_sql = "INSERT INTO user_key VALUES (%(value)s)"
+    key_list = generate(200)
+    conn.executemany(insert_sql,[dict(value=v) for v in key_list])
+
+def DeleteDate():
+    del_sql = "delete from user_key where id =2"
+    execute(del_sql)
+
+def QueryDate(conn):
+    sql ="select * from user_key"
+    rows = query(sql, conn)
+    printResult(rows)
+
+def printResult(rows):
+    if rows in None:
+        print "rows None"
+    for row in rows:
+        print row
+
+
+
 
 def getRandom():
     return "".join(random.sample(fild,4))
@@ -14,4 +87,5 @@ def generate(n):
     return [concatenate(5)  for i in range(n)]
 
 if __name__ == '__main__':
-    print generate(200)
+    dbconn = mysql_init(None)
+    process()
