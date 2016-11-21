10c9527298f4b2b91e892779847608eeaffcca45
luojiyin <luojiyin@hotmail.com>
2016-11-20 22:59:39 -0800

    python 产生200个序列号

---
+++ app_key.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#coding:utf-8
+import string, random
+
+field = string.letters + string.digits
+
+def getRandom():
+    return "".join(random.sample(field,4))
+
+def concatenate(group):
+    return "-".join([getRandom() for i in range(group)])
+
+def generate(n):
+    return [concatenate(4) for i in range(n)]
+
+if __name__ == '__main__':
+    print generate(200)
